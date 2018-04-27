84ac3fcb3cbabe870f5ecfa7d4493a8dfeb99ebc
dilowagner <diegowagner4@gmail.com>
2018-04-27 18:08:19 -0300

    Enviando atualizacoes

--- lib/api/audio.rb
+++ lib/api/audio.rb
@@ -8,10 +8,20 @@ module TotalVoice
       @client = client
     end
 
+    ##
+    # Envia um Audio para numero de destino
+    #
+    # @param [String] numero_destino
+    # @param [String] url_audio
+    # @param [Boolean] resposta_usuario
+    # @param [String] bina
+    # @param [Boolean] gravar_audio
+    # @return [json]
+    #
     def enviar(numero_destino, url_audio, resposta_usuario = false, bina = nil, gravar_audio = false)
       data = {
         body: {
-          numero_destino: numero_destino, # your columns/data
+          numero_destino: numero_destino,
           url_audio: url_audio,
           resposta_usuario: resposta_usuario,
           bina: bina,
@@ -21,6 +31,12 @@ module TotalVoice
       @client.post(ROTA_AUDIO, data)
     end
 
+    ##
+    # Busca as informações do audio
+    #
+    # @param [Integer] id
+    # @return [json]
+    #
     def buscar(id)
       @client.get(ROTA_AUDIO + "/" + id.to_s)
     end
